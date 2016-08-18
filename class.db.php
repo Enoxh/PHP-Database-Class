2c2c21e496f6be31fe7c01a87aa4ac89b180ca13
enoxh <enoxheloe@gmail.com>
2016-08-18 00:40:54 -0600

    bugfix

--- class.db.php
+++ class.db.php
@@ -1,88 +1,69 @@
 <?php
 class DBClass{
-
+//https://github.com/Enoxh/PHP-Database-Class
+//by @ENOXH
 
 function __construct(){
-
+//your db connection must establish $mysqli as connection variable
 include_once('db_conn.php');
-
-$this->mysqli = $mysqli;	
+$this->mysqli = $mysqli;
 }
-
 
 public function selectFromDb($selectors,$table){
-	
-	$sql = "SELECT $selectors FROM $table";
-	$result = mysqli_query($this->mysqli, $sql);
-
-	if (mysqli_num_rows($result) > 0) {
-	return $result;
-	}
-	} else {
-	return "0 results";
-	}	
-	
-}
-
+    $mysqli = $this->mysqli;
+    $sql = "SELECT $selectors FROM $table";
+    $result = $mysqli->query($sql);
+    if (mysqli_num_rows($result) > 0) {
+    return $result;
+    }
+    else {
+    return "0 results";
+    }
+    }
 
 public function selectFromDbWhere($selectors,$table,$whereField,$where){
-	
-	$sql = "SELECT $selectors FROM $table WHERE $whereField = $where";
-	$result = mysqli_query($this->mysqli, $sql);
-
-	if (mysqli_num_rows($result) > 0) {
-	return $result;
-	}
-	} else {
-	return "0 results";
-	}	
-	
-}
-
+    $mysqli = $this->mysqli;
+    $sql = "SELECT $selectors FROM `$table` WHERE $whereField = '$where'";
+    $result = $mysqli->query($sql);
+    if (mysqli_num_rows($result) > 0) {
+    return $result;
+    }
+    else {
+    return "0 results";
+    }
+    }
 
 public function insertIntoDb($selectors,$table,$values,$field,$fieldval){
-	
-	$sql = "INSERT INTO $table ($selectors) VALUES ($values) WHERE $field = $fieldval";
-	$result = mysqli_query($this->mysqli, $sql);
-
-	if (mysqli_num_rows($result) > 0) {
-	return $result;
-	}
-	} else {
-	return "0 results";
-	}	
-	
-}
+    $mysqli = $this->mysqli;
+    $sql = "INSERT INTO $table ($selectors) VALUES ($values) WHERE $field = $fieldval";
+    $result = $mysqli->query($sql);
+    if (mysqli_num_rows($result) > 0) {
+    return $result;
+    }
+    else {
+    return "0 results";
+    }
+    }
 
 public function updateDbWhere($selectors,$table,$updatevals,$field,$fieldval){
-	
-	$sql = "UPDATE $table SET $updatevals WHERE $field = $fieldval";
-	$result = mysqli_query($this->mysqli, $sql);
-
-	if (mysqli_num_rows($result) > 0) {
-	return $result;
-	}
-	} else {
-	return "0 results";
-	}	
-	
-}
-
+    $mysqli = $this->mysqli;
+    $sql = "UPDATE $table SET $updatevals WHERE $field = $fieldval";
+    $result = $mysqli->query($sql);
+    if (mysqli_num_rows($result) > 0) {
+    return $result;
+    }else {
+    return "0 results";
+    }
+    }
 public function deleteFromDB($table,$updatevals,$field,$fieldval){
-	
-	$sql = "DELETE FROM $table WHERE $field = $fieldval";
-	$result = mysqli_query($this->mysqli, $sql);
-
-	if (mysqli_num_rows($result) > 0) {
-	return $result;
-	}
-	} else {
-	return "0 results";
-	}	
-	
-}
-
-
-
-}
+    $mysqli = $this->mysqli;
+    $sql = "DELETE FROM $table WHERE $field = $fieldval";
+    $result = $mysqli->query($sql);
+    if (mysqli_num_rows($result) > 0) {
+    return $result;
+    }else {
+    return "0 results";
+    }
+    }
+}//end class
 ?>
